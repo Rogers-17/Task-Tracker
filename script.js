b0e9d2a2c251cd81c414410ffb7d16c6d6ae20d7
Rogers <mohammedrogers579@gmail.com>
2025-10-03 12:15:01 -0700

    Features/Added functions for adding tasks

--- script.js
+++ script.js
@@ -1,39 +1,54 @@
-let tasks = [
-    {
-        tasks: "Teach my students",
-        completed: false
-    },
-    {
-        tasks: "Learn new technology",
-        completed: false
-    },
-    {
-        tasks: "Complete homework",
-        completed: false
+const taskInput = document.getElementById('taskInput'); 
+const addTaskButton = document.getElementById('addTask');
+const taskList = document.getElementById('taskList');
+
+addTaskButton.addEventListener('click', function() {
+    const taskText = taskInput.value.trim();
+    if (taskText !== '') {
+        const taskItem = document.createElement('div');
+        taskItem.classList.add('task-item');
+        taskItem.innerHTML = `
+            <div>
+                <input type="checkbox" class="task-checkbox">
+                <span>${taskText}</span>
+            </div>
+            <div>
+                <button>Edit</button>
+                <button>Delete</button>
+            </div>
+        `;
+        taskList.appendChild(taskItem);
+        taskInput.value = '';
     }
-]
+});
 
-// const taskInput = document.getElementById('taskInput'); 
-// const addTaskButton = document.getElementById('addTask');
-// const taskList = document.getElementById('taskList');
 
-// addTaskButton.addEventListener('click', function() {
-//     const taskText = taskInput.value.trim();
-//     if (taskText !== '') {
-//         const taskItem = document.createElement('div');
-//         taskItem.classList.add('task-item');
-//         taskItem.innerHTML = `
-//             <div>
-//                 <input type="checkbox" class="task-checkbox">
-//                 <span>${taskText}</span>
-//             </div>
-//             <div>
-//                 <button>Edit</button>
-//                 <button>Delete</button>
-//             </div>
-//         `;
-//         taskList.appendChild(taskItem);
-//         taskInput.value = '';
+// let tasks = [
+//     {
+//         tasks: "Teach my students",
+//         completed: false
+//     },
+//     {
+//         tasks: "Learn new technology",
+//         completed: false
+//     },
+//     {
+//         tasks: "Complete homework",
+//         completed: false
 //     }
-// });
+// ]
+
+// console.log("before")
+// console.log("TASKS: ", tasks)
+
+// const addTask = (newTask) => {
+//     tasks.push(newTask);
+// }
+
+// console.log("after")
+// addTask({task: "Do my assignment", completed: false});
+
+// console.log("TASKS: ", tasks)
+
+
 
